5c7319d8766aba31765f3ba6cf18586e8c07df43
Oorthian Emissary <Vixenousvermingod@hotmail.com>
2017-02-15 15:49:12 +0100

    trying again

---
+++ Face/empty-example/sketch.js
@@ -0,0 +1,13 @@
+var dank = random(10, 20)
+
+function setup() {
+createCanvas(500, 500)
+}
+
+function draw() {
+    background(255, 155, 133);
+    noStroke();
+    fill(170, 246, 131);
+  ellipse(250, 250, 100, 100);
+    fill(255);
+}
