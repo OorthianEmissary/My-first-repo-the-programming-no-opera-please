db2412604698b362ec30c18d45e617c5e1ff84fe
Oorthian Emissary <Vixenousvermingod@hotmail.com>
2017-02-15 15:46:59 +0100

    This is really just a testy test. Air horn to come

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
-function setup() {
+var dank = random(10, 20)
 
+function setup() {
+createCanvas(500, 500)
 }
 
 function draw() {
-  Background(50)
-  Fill(100)
-  rect()
-}
-
-{
-    text(Hello World, 0, 0, 5, 5)
+    background(255, 155, 133);
+    noStroke();
+    fill(170, 246, 131);
+  ellipse(250, 250, 100, 100);
+    fill(255);
 }
